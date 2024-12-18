a93dac2d27fe468dc8805ff354495de2dd5fe12a
“Miniminiso” <minisominiso95@gmail.com>
2024-12-18 11:43:14 -0700

    added array

--- main.go
+++ main.go
@@ -8,5 +8,13 @@ func main() {
 	fmt.Println(a + b)
 	fmt.Println(a - b)
 	fmt.Println(a * b)
+	test2()
+}
 
+func test2() {
+	array1 := make([]int, 8)
+	array1[0] = 2
+	array1[1] = 9
+	array1[3] = 23
+	fmt.Println(array1)
 }
