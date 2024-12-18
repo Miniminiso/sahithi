4784924801318ffbdeb5d3ee919d44afdf3998d1
Miniminiso <minisominiso95@gmail.com>
2024-12-18 12:04:44 -0700

    adding branch (#1)
    * adding branch
    
    * multiplied
    
    * added array
    
    * added array
    
    ---------
    
    Co-authored-by: “Miniminiso” <“minisominiso95@gmail.com”>

--- main.go
+++ main.go
@@ -6,4 +6,15 @@ func main() {
 	var a int = 10
 	var b int = 20
 	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	test2()
+}
+
+func test2() {
+	array1 := make([]string, 8)
+	array1[0] = "sahi"
+	array1[1] = "swetha"
+	array1[3] = "harini"
+	fmt.Println(array1)
 }
